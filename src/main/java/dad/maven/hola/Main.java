6c15bae6aff2f618c6db93574b580a315ff508d4
moimah <moimah@outlook.com>
2019-09-17 16:08:14 +0200

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,15 @@
+package dad.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String greeting = StringUtils.reverse("Hola Maven");
+		
+		System.out.println(greeting);
+	}
+
+}
